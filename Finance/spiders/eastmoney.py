8a8ec570fef87732e31f144428f6e475684204f9
liang <passager@163.com>
2017-09-05 15:34:15 +0800

    编写的一部分

--- Finance/spiders/eastmoney.py
+++ Finance/spiders/eastmoney.py
@@ -11,7 +11,8 @@ class CrawlSpider(CrawlSpider):
     start_urls = ['http://www.eastmoney.com/']
 
     rules = (
-        Rule(LinkExtractor(allow=r'.*?eastmoney.com/.*'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=r'.*?eastmoney.com/list\,.*'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow='http://guba.eastmoney.com/news\,\d{6}\,\d{8,10}\.html'),callback='deal_page_contain_content',follow=True)
     )
 
     def parse_item(self, response):
@@ -24,5 +25,7 @@ class CrawlSpider(CrawlSpider):
         print 'hello'
 
 
-    def deal_page_contain_content(self):
+    def deal_page_contain_content(self,response):
+        pass
+        print response.xpath('//div[id="zwconttbt"]').extract()
         pass
